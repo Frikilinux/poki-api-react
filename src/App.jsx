b04978bb18da92a63df2fa69d8e48507a8145ab3
Frikilinux <frikilinux@gmail.com>
2023-04-08 20:07:14 -0300

    Render oh yeah

--- src/App.jsx
+++ src/App.jsx
@@ -15,23 +15,75 @@ const setNextURl = (data) => {
   return data.next
 }
 
-console.log('NEXXT URL', setNextURl(fetchPokemons(appState.currentURL)))
+const getPokemonsData = async () => {
+  const { next, results } = await fetchPokemons(appState.currentURL)
+  appState.currentURL = next
 
-console.log(await fetchPokemons(appState.currentURL))
+  const pokemonDataUrls = results.map((pokemon) => {
+    return pokemon.url
+  })
 
-const PokeCard = ({ image, name, experience, types, id, height, weight }) => {
+  const pokemonsData = await Promise.all(
+    pokemonDataUrls.map(async (url) => {
+      const nextPokemonsData = await fetch(url)
+      return await nextPokemonsData.json()
+    })
+  )
+
+  console.log('POKEMONES =>', pokemonsData)
+
+  return pokemonsData
+}
+
+console.log('POKEOMS DATA', await getPokemonsData())
+
+const pokemonTemplate = (pokemon) => {
+  return {
+    image: pokemon.sprites.other.home.front_default,
+    name: pokemon.name.toUpperCase(),
+    experience: pokemon.base_experience,
+    types: pokemon.types,
+    id: pokemon.id,
+    height: pokemon.height / 10,
+    weight: pokemon.weight / 10
+  }
+}
+
+const CreateTypeSpan = (types) => {
+  return types.map((tipo, index) => (
+    <span key={index} className={`${tipo.type.name} poke__type`}>{tipo.type.name}</span>
+  ))
+}
+
+// console.log('POKEMON', pokemonObject(fetchPokemons(appState.currentURL)));
+
+// console.log('NEXXT URL', setNextURl(await fetchPokemons(appState.currentURL)))
+
+// console.log(await fetchPokemons(appState.currentURL))
+
+const pokeCard = ({ image, name, experience, types, id, height, weight }) => {
   return (
   <div className="poke">
     <img src={image} />
     <h2>{name}</h2>
     <span className="exp">EXP: {experience}</span>
-    {/* <div className="tipo-poke">{createTypeSpan(types)}</div> */}
+    <div className="tipo-poke">
+      <CreateTypeSpan types={types}/>
+    </div>
     <p className="id-poke">#{id}</p>
     <p className="height">Height: {height}m</p>
     <p className="weight">Weight: {weight}Kg</p>
   </div>
   )
 }
+
+const PokeCardsSection = (pokemones) => (
+  pokemones.map(pokemon => {
+    return pokeCard(pokemonTemplate(pokemon))
+  })
+)
+
+// console.log('CARDS', pokeCardsSection(await getPokemonsData()))
 
 const Header = () => {
   return (
@@ -43,11 +95,37 @@ const Header = () => {
   )
 }
 
+const pokemonesData = await getPokemonsData()
+
 export function App () {
   return (
     <section>
       <Header/>
-      <PokeCard/>
+      <div className="poke__container" id="caja">{
+        pokemonesData.map((pokemon) => {
+          console.log('APP', pokemon)
+          const { image, name, experience, types, id, height, weight } = pokemonTemplate(pokemon)
+          return (
+            <div key={id} className="poke">
+              <img src={image} />
+              <h2>{name}</h2>
+              <span className="exp">EXP: {experience}</span>
+              <div className="tipo-poke">
+                {
+                  types.map((tipo, index) => {
+                    return <span key={index} className={`${tipo.type.name} poke__type`}>{tipo.type.name}</span>
+                  })
+                }
+              </div>
+              <p className="id-poke">#{id}</p>
+              <p className="height">Height: {height}m</p>
+              <p className="weight">Weight: {weight}Kg</p>
+            </div>
+          )
+        }
+        )
+      }
+      </div>
     </section>
 
   )
